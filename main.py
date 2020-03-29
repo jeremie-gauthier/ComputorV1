9429be9a86b8653e91ffe7d751764816906bb3cf
Jeremie <jeremiegthr@gmail.com>
2020-03-29 21:24:21 +0200

    unix friendly

--- main.py
+++ main.py
@@ -7,21 +7,19 @@ from solver import *
 def main(arg):
     try:
         numbers = parser(arg)()
-        print("numbers =>", numbers)
         result = solver(numbers)
         return 0
 
     except Exception as e:
         print(f"[-] {e}", file=sys.stderr)
-        print(f"[*] Exit", file=sys.stderr)
         return -1
 
 
 if __name__ == "__main__":
     argc = len(sys.argv)
     if argc == 2:
-        main(sys.argv[1])
+        if main(sys.argv[1]) < 0:
+            sys.exit("[*] Exit")
     else:
         print("[-] Arguments error", file=sys.stderr)
-        print("[*] Exit", file=sys.stderr)
-        sys.exit(-1)
+        sys.exit("[*] Exit")
