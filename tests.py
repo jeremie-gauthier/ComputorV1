108b6bd41dcbba836891fdd4d26dd89c7a171564
Jeremie <jeremiegthr@gmail.com>
2020-04-02 11:10:03 +0200

    more tests

--- tests.py
+++ tests.py
@@ -28,15 +28,43 @@ class TestService(unittest.TestCase):
     def assertEqual(self, case, expected):
         with open(os.devnull, "w") as devnull:
             with contextlib.redirect_stdout(devnull):
-                super().assertEqual(main(case), expected)
+                result = main(case)
+                super().assertEqual(result["status"], expected["status"])
+                if result["status"] == "Success":
+                    if result["solutions"] is None:
+                        super().assertEqual(result["solutions"], expected["s1"])
+                    elif len(result["solutions"]) == 2:
+                        s1, s2 = result["solutions"]
+                        super().assertEqual(s1, expected["s1"])
+                        super().assertEqual(s2, expected["s2"])
+                    else:
+                        super().assertEqual(result["solutions"][0], expected["s1"])
 
     def runAll(self):
         self.runFormattingTests(filename="formatting")
+        self.runResultTests(filename="results")
 
     @getTestFile
     @silentErrorLog
     def runFormattingTests(self, tests):
         print("[*] TESTS FORMATTING")
+        res = [" "] * len(tests) * 2
+        done = 0
+        for test in tests:
+            print(f"\r[{''.join(res)}]", end="")
+            try:
+                self.assertEqual(test["case"], test["expected"])
+                res[done] = "✔"
+            except Exception:
+                res[done] = "✖"
+            finally:
+                done += 2
+        print(f"\r[{''.join(res)}]")
+
+    @getTestFile
+    # @silentErrorLog
+    def runResultTests(self, tests):
+        print("[*] TESTS RESULTS")
         res = [" "] * len(tests) * 2
         done = 0
         for test in tests:
