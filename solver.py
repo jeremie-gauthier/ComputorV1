28156720c295685ff48be980f2052b9999ad8124
Jeremie <jeremiegthr@gmail.com>
2020-04-02 11:10:49 +0200

    add 1st degree solutions

--- solver.py
+++ solver.py
@@ -7,7 +7,7 @@ def expr_reducer(coefs: map) -> List[float]:
     return list(map(lambda c: sub(*c), zip(*coefs)))
 
 
-def solver(coefs: List[float]) -> dict:
+def solver(coefs: List[float], degree: int) -> dict:
     def get_delta(a: float, b: float, c: float) -> float:
         return b ** 2 - 4 * a * c
 
@@ -17,11 +17,19 @@ def solver(coefs: List[float]) -> dict:
         if delta < 0:
             result = None
         elif delta == 0:
-            result = -b / 2 * a
+            result = (-b / (2 * a),)
         else:
             s1 = (-b - delta ** 0.5) / (2 * a)
             s2 = (-b + delta ** 0.5) / (2 * a)
             result = (s1, s2)
         return {"delta": delta, "result": result}
 
-    return second_degree()
+    def first_degree() -> dict:
+        b, a = coefs
+        result = -b / a
+        return {"delta": None, "result": result}
+
+    if degree == 1:
+        return first_degree()
+    else:
+        return second_degree()
--- verbose.py
+++ verbose.py
@@ -25,10 +25,13 @@ def vb_reduced_form(reduced: list) -> str:
 
 
 def vb_result(delta: float, result: Any) -> str:
-    if delta < 0:
-        return "Discriminant is strictly negative, there is no solution"
-    elif delta == 0:
-        return f"Discriminant is equal to zero, the only solution is:\n{result}"
+    if delta is None:
+        return f"The solution is: {result}"
     else:
-        return f"Discriminant is strictly positive, the two solutions are:\n\
-                \rS1 = {result[0]: .6f}\nS2 = {result[1]: .6f}"
+        if delta < 0:
+            return "Discriminant is strictly negative, there is no solution"
+        elif delta == 0:
+            return f"Discriminant is equal to zero, the only solution is:\n{result[0]}"
+        else:
+            return f"Discriminant is strictly positive, the two solutions are:\n\
+                    \rS1 = {result[0]: .6f}\nS2 = {result[1]: .6f}"
