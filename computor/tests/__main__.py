c05b2da4616986d60180ce11a709c120bdf9cf68
Jeremie <jeremiegthr@gmail.com>
2020-04-08 11:10:34 +0200

    make it more readable

--- computor/tests/__main__.py
+++ computor/tests/__main__.py
@@ -1,6 +1,14 @@
 import sys
 from .runner import TestService
 
+synopsis = """The following tests are available:
+- format
+- result
+
+e.g.: py -m computor.tests [test.name ...]
+You can run all of them by omitting <test.name>"""
+
+
 if __name__ == "__main__":
     runner = TestService()
     if len(sys.argv) > 1:
@@ -10,15 +18,9 @@ if __name__ == "__main__":
             elif arg.lower() in ("result", "results"):
                 runner.runResultTests(filename="results")
             elif arg.lower() in ("-h", "--help"):
-                print(
-                    """The following tests are available:
-                    \r- format
-                    \r- result
-
-                    \re.g.: py -m computor.tests [test.name ...]
-                    \rYou can run all of them by omitting <test.name>"""
-                )
+                print(synopsis)
             else:
-                print(f"[-] Unrecognized arg '{arg}'\n[*] Skipped", file=sys.stderr)
+                print(f"[-] Unrecognized arg '{arg}'", file=sys.stderr)
+                print("[*] Skipped")
     else:
         runner.runAll()
