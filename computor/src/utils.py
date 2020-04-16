b0e7e4a21c4a3dfe80f9eaa65e0b323943e11f52
Jeremie <jeremiegthr@gmail.com>
2020-04-16 19:30:30 +0200

    english version

--- computor/src/utils.py
+++ computor/src/utils.py
@@ -1,11 +1,11 @@
-def pgcd(a: int, b: int) -> int:
+def gcd(a: int, b: int) -> int:
     if a == b:
         return a
     else:
         if a > b:
-            return pgcd(a - b, b)
+            return gcd(a - b, b)
         else:
-            return pgcd(a, b - a)
+            return gcd(a, b - a)
 
 
 def is_neg(x: float):
