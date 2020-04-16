bea12db86fc37da261d32c04256a8401c0022360
Jeremie <jeremiegthr@gmail.com>
2020-04-16 19:11:42 +0200

    add arg_parser

--- computor/__main__.py
+++ computor/__main__.py
@@ -3,8 +3,13 @@ import sys
 
 if __name__ == "__main__":
     argc = len(sys.argv)
-    if argc == 2:
-        ret = app.run(sys.argv[1])
+    if argc > 1 and argc < 4:
+        entry = sys.argv[1]
+        if argc == 3 and sys.argv[2].startswith("-"):
+            verbose = "v" in sys.argv[2]
+            ret = app.run(entry, opt_verbose=verbose)
+        else:
+            ret = app.run(entry)
         if ret["status"] == "Error":
             sys.exit("[*] Exit")
     else:
