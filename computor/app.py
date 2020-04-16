0d9c7dc3d63d11de3b80bcfd286d5ef923181782
Jeremie <jeremiegthr@gmail.com>
2020-04-16 19:12:10 +0200

    add verbose option to show mid-step computations

--- computor/app.py
+++ computor/app.py
@@ -2,16 +2,18 @@ import sys
 from computor.src import parser, sanitizer, solver, verbose, reducer
 
 
-def run(arg: str) -> int:
+def run(entry: str, opt_verbose: bool = False) -> int:
     try:
-        equation = sanitizer.sanitize_entry(arg)
+        equation = sanitizer.sanitize_entry(entry)
 
         # Organize expr to ease computations
         split_eq = parser.split_equality(equation)
         coefs = map(lambda eq: parser.parser(eq), split_eq)
 
         # Start equation resolution
-        reduced_form = reducer.expression(coefs)
+        reduced_form, mid_steps = reducer.expression(coefs, opt_verbose)
+        if opt_verbose:
+            print("\n".join(mid_steps))
         degree = parser.get_degree(reduced_form)
         print("\n".join([verbose.reduced_form(reduced_form), verbose.degree(degree)]))
         delta, result = solver.solver(reduced_form, degree).values()
