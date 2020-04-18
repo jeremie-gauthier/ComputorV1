489895f909caa2f79539661c9d0080c8466a96a6
Jeremie <jeremiegthr@gmail.com>
2020-04-18 12:18:27 +0200

    add multithread class

--- computor/src/utils.py
+++ computor/src/utils.py
@@ -1,3 +1,20 @@
+from concurrent.futures import ThreadPoolExecutor
+from typing import List, Any, Callable
+
+
+class Parallelize:
+    def __init__(self):
+        self.executor = ThreadPoolExecutor(max_workers=4)
+        self.running_tasks = []
+
+    def execute(self, tasks: List[Callable]):
+        for task in tasks:
+            self.running_tasks.append(self.executor.submit(task))
+
+    def get_results(self) -> List[Any]:
+        return [task.result() for task in self.running_tasks]
+
+
 def gcd(a: int, b: int) -> int:
     while b != 0:
         a, b = b, a % b
